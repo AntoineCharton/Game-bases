ad28b4c8cf228125c3fd650cd8e5e23f26165251
acharton <antoine.charton@gmail.com>
2018-09-03 23:44:10 -0400

    Added gc collect Added Disable On trigger stay

---
+++ Game/Utils/DisableOnTriggerStay.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class DisableOnTriggerStay : MonoBehaviour
+{
+    public GameObject ObjectToEnable;
+
+      void OnTriggerExit(Collider collision)
+    {
+        ObjectToEnable.SetActive(true);
+    }
+
+	
+    void OnTriggerStay(Collider other)
+	{
+		 ObjectToEnable.SetActive(false);
+	}
+}
---
+++ Game/Utils/GCCollector.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class GCCollector : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		if(Time.frameCount % 30 == 0) {
+			System.GC.Collect();
+		}
+	}
+}
