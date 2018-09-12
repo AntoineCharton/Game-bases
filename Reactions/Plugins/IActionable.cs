1b5314d0675d84145e7cc690065e6f5549c352d3
acharton <antoine.charton@gmail.com>
2018-09-12 16:36:59 -0400

    removed unused namespaces

--- Reactions/Plugins/IActionable.cs
+++ Reactions/Plugins/IActionable.cs
@@ -1,6 +1,4 @@
-﻿using UnityEngine;
-
-public interface IActionable
+﻿public interface IActionable
 {
     void Register(Actions.TriggerAction triggerAction);
     void Unregister(Actions.TriggerAction triggerAction);
