293c000390a6627468c2e67899ae32b4cc93af9a
acharton <antoine.charton@gmail.com>
2019-03-14 21:39:46 -0400

    Added rotate on trigger enter

---
+++ Game/Utils/RotateOnTriggerEnter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class RotateOnTriggerEnter : MonoBehaviour {
+    public string TagTrigger = "Player";
+    public GameObject ObjectToRotate;
+    public Vector3 TargetRotation;
+    public Vector3 StartRotation;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == TagTrigger)
+            ObjectToRotate.transform.DOLocalRotate(TargetRotation, 0.3f);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == TagTrigger)
+            ObjectToRotate.transform.DOLocalRotate(StartRotation, 0.3f);
+    }
+}
