2cc23aa73d42ba9373fd2dadc5f0327d3778caaa
acharton <antoine.charton@gmail.com>
2019-12-11 09:17:07 -0500

    Changed initialize shader on awake

--- Shaders/ChangeMaterialShaderOnStart.cs
+++ Shaders/ChangeMaterialShaderOnStart.cs
@@ -8,7 +8,7 @@ public class ChangeMaterialShaderOnStart : MonoBehaviour
     public List<ShaderSwitcher> ShaderSwitcher;
 
     // Use this for initialization
-    void Start()
+    void Awake()
     {
         SetToStart();
 
